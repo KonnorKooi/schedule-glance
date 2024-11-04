ec0defe45d2cf6b4d7af2a553c091b68d19b78ca
konnorkooi <kooik2@wwu.edu>
2024-11-03 16:21:45 -0800

    all good

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import './styles/Schedule.css';
 console.log('Schedule CSS imported');
 
+import './styles/index.css';
 export { default as Schedule } from './components/Schedule';
 export { default as ScheduleHeader } from './components/ScheduleHeader';
 export { default as ScheduleCell } from './components/ScheduleCell';
---
+++ test-schedule/index.js
@@ -0,0 +1,2 @@
+import { Schedule } from '@konnorkooi/schedule-glance';
+import '@konnorkooi/schedule-glance/dist/index.css';
